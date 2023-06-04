075a8c0c59df9d973ef414919a3f867e0b5e79f0
oleg <s1ckoleg@mail.ru>
2023-06-04 23:40:52 +0300

    module 3.2

--- src/presenter/Presenter.js
+++ src/presenter/Presenter.js
@@ -3,15 +3,21 @@ import EventView from '../view/EventView.js';
 import SortView from '../view/SortView.js';
 import {generateEvents} from '../mock/event';
 import {getRandomInt} from '../utils.js';
+import EmptyListView from '../view/EmptyListView.js';
 
 class Presenter {
   init(container) {
     this.container = container;
-    this.events = generateEvents(getRandomInt(1, 3));
+    this.events = generateEvents(getRandomInt(0, 3));
     this.eventViews = this.events.map((data) => new EventView(data));
     render(new SortView(), this.container);
-    for (let i = 0; i < this.eventViews.length; i++) {
-      render(this.eventViews[i], this.container);
+
+    if (this.events.length === 0) {
+      render(new EmptyListView(), this.container);
+    } else {
+      for (let i = 0; i < this.eventViews.length; i++) {
+        render(this.eventViews[i], this.container);
+      }
     }
   }
 }
